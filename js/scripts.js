638536f3c868402a7f84689cc703b9ca0ffed0f8
heather knight and haley swain <heather@>
2016-10-03 19:13:26 -0700

    complete business logic

--- js/scripts.js
+++ js/scripts.js
@@ -12,10 +12,10 @@ var leapYear = function(year) {
   return false;
 };
 
-var leapYear = function(year) {
-  if (year % 4 === 0) {
-    return true;
-  } else {
-    return false;
-  }
-};
+var leapYear = function (year) {
+    if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+      return true;
+    } else {
+      return false;
+    }
+  };
